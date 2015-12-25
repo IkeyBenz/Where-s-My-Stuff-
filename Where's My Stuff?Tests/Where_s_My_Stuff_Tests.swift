60de8a1dc07cf3df307ac7650f4bc333b6fb1c14
Ikey Benzaken <ikey.benz@gmail.com>
2015-12-25 14:36:39 -0500

    Initial Commit

---
+++ Where's My Stuff?Tests/Where_s_My_Stuff_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Where_s_My_Stuff_Tests.swift
+//  Where's My Stuff?Tests
+//
+//  Created by Ikey Benzaken on 12/25/15.
+//  Copyright © 2015 Ikey Benzaken. All rights reserved.
+//
+
+import XCTest
+@testable import Where_s_My_Stuff_
+
+class Where_s_My_Stuff_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
